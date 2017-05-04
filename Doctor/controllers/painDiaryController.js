0f06c073cc9b79e82b12834ac72b9b82b7531384
orshe4 <orshe4@gmail.com>
2017-05-04 22:38:30 +0300

    There is graph!

--- Doctor/controllers/painDiaryController.js
+++ Doctor/controllers/painDiaryController.js
@@ -1,6 +1,6 @@
 angular.module('doctorApp')
     .controller('painDiaryController', ['$scope', 'Reports', function ($scope, Reports) {
-        
+
         $scope.reports = [];
 
         Reports.get({
@@ -12,5 +12,34 @@ angular.module('doctorApp')
         $scope.try = function () {
             console.log("a");
         }
+
+
+        var trace1 = {
+            x: [1, 2, 3, 4],
+            y: [10, 15, 13, 17],
+            mode: 'markers'
+        };
+
+        var trace2 = {
+            x: [2, 3, 4, 5],
+            y: [16, 5, 11, 10],
+            mode: 'lines'
+        };
+
+        var trace3 = {
+            x: [1, 2, 3, 4],
+            y: [12, 9, 15, 12],
+            mode: 'lines+markers'
+        };
+
+        var data = [trace1, trace2, trace3];
+
+        var layout = {
+            title: 'Line and Scatter Plot',
+            height: 400,
+            width: 480
+        };
+
+        Plotly.newPlot('myDiv', data, layout);
 
     }]);
