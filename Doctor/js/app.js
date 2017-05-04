7fc0022322319eac8b1c27fdd5df8a4c40f2637f
Daniel Pleshakov <Daniel Pleshakov>
2017-05-04 16:32:20 +0300

    Add ng-routes and integration

--- Doctor/js/app.js
+++ Doctor/js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('doctorApp', ['ngRoute', 'ngMaterial']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/user', {
+            templateUrl: './views/user.html',
+            controller: 'userController'
+        })
+        .when('/', {
+            templateUrl: './views/home.html',
+            controller: 'mainController'
+        });
+});
